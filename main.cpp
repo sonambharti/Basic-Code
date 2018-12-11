6c4a9cc262ffd71f3030e1639a0924fd067c1794
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2018-12-11 13:50:02 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,3 @@
-/*#include<iostream>
-using namespace std;
-int print(int n)
-{
-    if(n<=0)
-    return 1;
-    else
-    {
-        int p=0, result;
-        while(n--)
-        {
-            result=print(p++);
-            cout<<result<<" ";
-        }
-        return result;
-
-    }
-
-}
-int main()
- {
-	//code
-	int T;
-	cin>>T;
-	while(T--)
-	{
-	    int N;
-	    cin>>N;
-	    print(N);
-	    cout<<endl;
-	}
-	return 0;
-}
-*/
 #include<iostream>
 using namespace std;
 int print(int n)
