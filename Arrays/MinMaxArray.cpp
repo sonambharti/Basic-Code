0224bb192a2a4e314a8443a91dca87d57a85781d
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-03-27 17:45:10 +0530

    Mina and Max array Elements

--- Arrays/MinMaxArray.cpp
+++ Arrays/MinMaxArray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int n, i, min, max;
-    cout<<"Enter array size"<<endl;
+    cout<<"Enter array size: "<<endl;
     cin>>n;
     int arr[n];
     cout<<"Enter the array elements: "<<endl;
