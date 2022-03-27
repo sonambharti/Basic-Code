49e0d68350032c068f8e5061c4860134c84dba53
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-03-27 18:58:27 +0530

    maximum perimeter

---
+++ Basics/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int print(int n)
+{
+    if(n<=0)
+        return 1;
+    print(n-1);
+    cout<<n<<" ";
+}
+int main()
+{
+    int N;
+    cin>>N;
+    print(N);
+    
+    return 0;
+}
+
+
+
+
+
+
+
