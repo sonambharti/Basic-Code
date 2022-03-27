4353cc25a32c43a1514f17700b7359f430d9e830
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-03-27 18:10:48 +0530

    Change 0 to 5

---
+++ Basics/0to5.cpp
@@ -0,0 +1,14 @@
+#include"0to5.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter any number : ";
+    cin>>num;
+   int convertednum=convert(num);
+   cout<<convertednum<<endl;
+   return 0;
+
+
+}
