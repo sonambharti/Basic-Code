41bc5673651ebcef0b049ef9ad362002ae71b3cd
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-07-06 02:14:09 +0530

    find valid permutation for repeated digits

---
+++ Basics/ValidPermute2.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> ans;
+
+void permute(vector<int> &a, int indx){
+    if(indx == a.size()){
+        ans.push_back(a);
+        return;
+    }
+    
+    for(int i = indx; i<a.size();i++){
+        if(i!=indx and a[i] == a[indx])
+            continue;
+        swap(a[i],a[indx]);
+        permute(a,indx+1);
+        swap(a[i],a[indx]);
+        
+    }
+    return;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    
+    for(auto &i : a){
+        cin>>i;
+    }
+    permute(a,0);
+    
+    for(auto v : ans){
+        for(auto i : v){
+            cout<<i<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
